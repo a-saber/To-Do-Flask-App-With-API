9693e2bfc1cac95e2377dfae7e7c69d1d6fca5bf
a-saber <ahs629862@gmail.com>
2024-06-21 09:58:49 -0400

    complete test functions

--- tests/test_app.py
+++ tests/test_app.py
@@ -18,3 +18,32 @@ def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b'To-Do List' in response.data
+
+def test_add_todo(client):
+    """Test adding a new todo."""
+    response = client.post('/add', data={'title': 'New Todo'})
+    assert response.status_code == 302  # Redirect status
+    with app.app_context():
+        todo = Todo.query.filter_by(title='New Todo').first()
+        assert todo is not None
+
+def test_get_todos_api(client):
+    """Test the /api/todos endpoint."""
+    with app.app_context():
+        db.session.add(Todo(title='Sample Todo'))
+        db.session.commit()
+    response = client.get('/api/todos')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert len(data) == 1
+    assert data[0]['title'] == 'Sample Todo'
+
+def test_create_todo_api(client):
+    """Test creating a todo via the API."""
+    response = client.post('/api/todos', json={'title': 'API Todo'})
+    assert response.status_code == 201
+    data = response.get_json()
+    assert data['title'] == 'API Todo'
+    with app.app_context():
+        todo = Todo.query.filter_by(title='API Todo').first()
+        assert todo is not None
